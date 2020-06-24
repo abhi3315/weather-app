3a5903199fafd22b5a5b84e6e225ba3a88f8bbe7
Abhishek Sharma <abhi243461@hotmail.com>
2020-06-24 19:31:45 +0530

    feat: Render data to web page

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,33 @@ console.log("Client side javascript is loaded.")
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const contentDiv = document.querySelector('.weather')
+const loader = '<div class="loader"></div>'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    contentDiv.innerHTML = loader
     const address = search.value
 
     fetch('http://localhost:3000/weather?address=' + address)
         .then(response => {
             response.json().then(data => {
                 if (data.error) {
-                    console.log(data.error)
+                    const errorNode = `<h1 class="error">${data.error}</h1>`
+                    contentDiv.innerHTML = errorNode
                 } else {
-                    console.log(data)
+                    const weatherNode =
+                        `<div class="weather-details">
+                            <img class="weather-img"
+                                src=${data.weather_icons[0]}>
+                            <h5>${data.weather_descriptions[0]}</h5>
+                            <h3>${data.location}</h3>
+                            <h1>${data.temperature} °C</h1>
+                            <h4>Humidity: ${data.humidity}</h4>
+                            <h4>Wind Speed: ${data.wind_speed} KM/H</h4>
+                            <h4>Wind Direction: ${data.wind_dir}</h4>
+                        </div>`
+                    contentDiv.innerHTML = weatherNode
                 }
             })
         })
